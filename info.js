b842dd73228f311fcb51d1a63b1370f6cd889202
ronaldoaf <ronaldo.a.f@gmail.com>
2016-08-23 17:00:27 -0300

    Update and rename info.txt to info.js

---
+++ info.js
@@ -0,0 +1,46 @@
+//section.qb-QuickBetModule 
+
+
+set_stake_show=$('section.qb-QuickBetModule ').hasClass('qb-QuickBetModule_SetStake')
+
+
+
+normalRandomInt=function(min,max){
+
+	var gaussian=function(mean, stdev) {
+		var y2;
+		var use_last = false;
+		return function() {
+			var y1;
+			if(use_last) {
+			   y1 = y2;
+			   use_last = false;
+			}
+			else {
+				var x1, x2, w;
+				do {
+					 x1 = 2.0 * Math.random() - 1.0;
+					 x2 = 2.0 * Math.random() - 1.0;
+					 w  = x1 * x1 + x2 * x2;               
+				} while( w >= 1.0);
+				w = Math.sqrt((-2.0 * Math.log(w))/w);
+				y1 = x1 * w;
+				y2 = x2 * w;
+				use_last = true;
+		   }
+
+		   var retval = mean + stdev * y1;
+		   if(retval > 0) 
+			   return retval;
+		   return -retval;
+	   }
+	}
+	
+	var r=Math.round(gaussian((max-min)/2.0, (max-min)/6.0)());
+	
+	if (r>max) r=max;
+	if (r<min) r=min;
+	
+	return r;
+
+}
