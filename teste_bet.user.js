18071949b4c90887693cb82b683c24b7c23c98c4
ronaldoaf <ronaldo.a.f@gmail.com>
2016-10-02 01:17:39 -0300

    Update teste_bet.user.js

--- teste_bet.user.js
+++ teste_bet.user.js
@@ -29,7 +29,13 @@ jQuery.fn.extend({
 
 
 
-unsafeWindow.bot={}
+unsafeWindow.bot={};
+
+bot.defs={
+    stake: 1.00
+};
+
+
 
 
 bot.seq=function(funcs){
@@ -38,6 +44,8 @@ bot.seq=function(funcs){
 		tempo+=e.t;
 	    setTimeout(e.f, tempo );
 	});
+	return tempo;
+	
 };
 
 
@@ -66,22 +74,30 @@ bot.jogoLive = function (home,away){
 			($(e).find('.ipe-ParticipantCouponFixtureName_TeamName:eq(1)').html()==away )
 		){
 			jogo.positionInMarket=i;
-			jogo.posSelsJogo=[jogo.positionInMarket, jogo.positionInMarket+jogo.numJogos]
+			jogo.posSelsJogo=[jogo.positionInMarket, jogo.positionInMarket+jogo.numJogos];
 			jogo.selHome=$(jogo.market).find('.ip-Participant').eq(jogo.posSelsJogo[0]);
 			jogo.selAway=$(jogo.market).find('.ip-Participant').eq(jogo.posSelsJogo[1]);				
 		}
 	});
+	jogo.tempo=Number($(jogo.market).find('.ipe-ParticipantCouponFixtureName_Timer').text().split(':')[0]);
+	
 	jogo.betHome=function(){ jogo.selHome.click();   };
 	jogo.betAway=function(){ jogo.selAway.click();   };
 	
 	return jogo;
-}
-
-
-
-bot.defs={
-    stake: 1.00
 };
+
+
+
+bot.jaFoiApostado=function(home,away){
+	 var saida=false;
+     $(bot.myBets).each(function(i,e){
+         if(home+' v '+away==e.jogo) saida=true;
+	 });
+	
+	return saida;
+};
+
 
 
 bot.setStake=function(valor){
@@ -89,9 +105,6 @@ bot.setStake=function(valor){
 	    console.log(digito);
 		$('.qb-KeypadButton:contains('+digito+')').click();		
 	};
-	
-    ;
-	
 
 	
 	
@@ -101,9 +114,52 @@ bot.setStake=function(valor){
 	    lista_seq.push({ f:(function(){ digita(digito); }), t:500 }  );
 	});
 	
-	//Digita na sequencia, com intervalo de tempo
-	bot.seq(lista_seq);
+	//Digita na sequencia, com intervalo de tempo. Retorna o tempo total
+	return bot.seq(lista_seq);
 };
+
+
+
+
+bot.apostar=function(selObj){
+	 //Se já houve alguma aposta em andamento. NÃO APOSTA
+     if ($('.qb-QuickBetModule').hasClass('qb-QuickBetModule_BetSelected') ) return -1;
+	
+	
+	
+	 //Se estiver suspenso não NÃO APOSTA
+	 if ( selObj.hasClass('ip-Participant_Suspended') ) return -1;
+	
+	
+	
+	 selObj.click();
+      
+	 //Se não tem valor setado OU o stake setado é diferente do definido
+	 if(  ($('.qb-QuickBetModule').hasClass('qb-QuickBetModule_NoValue') ) ||    (bot.stake!=Number($('.qb-StakeBox ').text()) ) ) {
+         console.log('SETA STAKE');
+		 
+		 var tempo_para_placeBet=2000;
+		 setTimeout(function(){
+              tempo_para_placeBet+=bot.setStake(bot.defs.stake);
+		 },1000);
+		 
+		 setTimeout(function(){
+              $('.qb-PlaceBetButton').click();
+		 },tempo_para_placeBet);
+		 
+		 
+		 
+	 }
+
+	 
+
+	 
+	
+	
+	
+};
+
+
 
 
 
@@ -122,6 +178,7 @@ bot.onMyBets=function(){
             score_inicial: $(e).find('.myb-OpenBetParticipant_HeaderTitle').text().split(')')[0].split('(')[1],  
             stake: Number( $(e).find('.myb-OpenBetItem_StakeText').text() ),
             toReturn: Number( $(e).find('.myb-OpenBetItem_ReturnText').text() ),
+			
 
         };
         myBets.push(bet);
@@ -152,10 +209,103 @@ bot.onCouponAsianHalf=function(){
 
 bot.onCoupon=function(){
     //console.log('ok');
-   if ($('.ipe-EventViewTitle_Text').text()=='Asians In-Play') bot.onCouponAsianFull();
-   if ($('.ipe-EventViewTitle_Text').text()=='1st Half Asians In-Play') bot.onCouponAsianHalf();
-    
-    
+   //if ($('.ipe-EventViewTitle_Text').text()=='Asians In-Play') bot.onCouponAsianFull();
+   //if ($('.ipe-EventViewTitle_Text').text()=='1st Half Asians In-Play') bot.onCouponAsianHalf();
+   
+   var ahSel=function(selObj){
+       var arr_ah=selObj.find('.ip-Participant_OppName').text().split(',');
+ 
+	   var s=0;
+	   $(arr_ah).each(function(i,e){ s+=Number(e); });
+	   return s/$(arr_ah).size();
+   };	
+	
+	
+   var onLoadStats=function(response){
+       //console.log(response);
+	   var jogos=eval(response.responseText);
+	   console.log(jogos);
+	   
+	   
+	   //Para jogo no cupom
+	   $('.ipe-ParticipantCouponFixtureName_Participant').each(function(i,e){
+
+		   var home=$(e).find('.ipe-ParticipantCouponFixtureName_TeamName:eq(0)').text();
+		   var away=$(e).find('.ipe-ParticipantCouponFixtureName_TeamName:eq(1)').text();
+		   
+		   //Cada jogo do Ajax
+		   $(jogos).each(function(ii,jogo){
+                 //console.log([jogo.home, jogo.away, away] );
+		         if(  (jogo.home==home) && (jogo.away==away) ){
+				       //console.log(['bateu', home, away]);
+					   jogo_selecionado=bot.jogoLive(home,away);
+					   
+					    if ($('.ipe-EventViewTitle_Text').text()=='Asians In-Play') {
+						       if (jogo_selecionado.tempo<70) return; 
+						}
+					    if ($('.ipe-EventViewTitle_Text').text()=='1st Half Asians In-Play'){
+						        if (jogo_selecionado.tempo<28) return; 
+						}
+					   
+					 
+					    //Aposta no Home
+					    if (
+							( jogo.ind>2.00 ) &&  
+							( jogo.ind2>1.00) && 
+							( ahSel(jogo_selecionado.selHome)>=0.0)  
+						){
+						     if ( !bot.jaFoiApostado(home,away) ){
+								 bot.apostar(jogo_selecionado.selHome);
+								 console.log(jogo);
+								 console.log('APOSTANDO NO HOME');
+						     }
+						};
+			   
+			            
+                        //Aposta no Away
+					    if (
+							( jogo.ind<-2.00 ) &&  
+							( jogo.ind2<-1.00) && 
+							( ahSel(jogo_selecionado.selAway)>=0.0)  
+						){
+						     if (!bot.jaFoiApostado(home,away)){
+								 bot.apostar(jogo_selecionado.selAway);
+								 console.log(jogo);
+								 console.log('APOSTANDO NO AWAY');
+						     }
+						};		   
+			             
+			   
+
+				 }
+		   });
+		   
+
+	   });
+	   
+	   
+   };
+   
+   var time_=Math.floor( (+new Date) /1000);
+
+	if ( !(time_ % 10) ) {
+	   //console.log('ok');
+	   GM_xmlhttpRequest({
+		   method: "GET",
+		   url: "http://aposte.me/live/stats.php?t="+time_,
+		   headers: { 
+			   'Accept': "*/*; charset=utf-8",
+		   },
+		   onload: onLoadStats, 
+	   });  
+   
+   }
+   
+	
+	//Se foi apostado com sucesso fecha o modula QB, clicando no OK
+	if ($('.qb-QuickBetModule').hasClass('qb-QuickBetModule_Placed') ) {
+         $('.qb-MessageContainer_Indicator').click(); 
+	}
 
 };
 
@@ -187,7 +337,7 @@ setInterval(function(){
     }
 
    
-},500);
+},1000);
 
 
 
