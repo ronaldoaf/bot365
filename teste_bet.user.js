3361be63577f6dd5d340e73012e0bea721044783
ronaldoaf <ronaldo.a.f@gmail.com>
2016-10-26 14:10:16 -0200

    Update teste_bet.user.js

--- teste_bet.user.js
+++ teste_bet.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         teste_bet
 // @namespace    http://aposte.me/
-// @version      0.1.18
+// @version      0.1.19
 // @description  try to take over the world!
 // @author       Ronaldo
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
