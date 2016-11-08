39c01d43d9708c8e6622101a32ca78ebd3af9f67
ronaldoaf <ronaldo.a.f@gmail.com>
2016-11-08 16:54:44 -0200

    Create totalcorner.js

---
+++ totalcorner.js
@@ -0,0 +1,40 @@
+var TABELA_JOGOS={};
+$('[data-match_id]').each(function(i,e){  
+	TABELA_JOGOS[$(e).attr('data-match_id')]={
+			home: $(e).find('.match_home a').text(),
+			away: $(e).find('.match_away a').text()
+	};			
+});
+
+$( document ).ajaxComplete(function( event, res, settings) {
+	if (!settings.url.includes('api_ongoing_matches') ) return;
+	
+	var DATA=[];
+    $(res.responseJSON).each(function(i,j){
+		
+		HA=$(j.han.split(',')).size()==2 ? (Number(j.han.split(',')[0]) + Number(j.han.split(',')[1]))/2.0 : Number(j.han.split(',')[0]);
+		DATA.push({
+				id:   j.id,
+				time_:j.sta,
+				home: TABELA_JOGOS[j.id].home,
+				away: TABELA_JOGOS[j.id].away,
+				HA:   HA,
+				gH:   Number(j.hg),
+				gA:   Number(j.ag),
+				daH:  Number(j.hda), 
+				daA:  Number(j.ada), 
+				soH:  Number(j.ho), 
+				soA:  Number(j.ao), 
+				sfH:  Number(j.hf),
+				sfA:  Number(j.af)
+		});
+		
+		
+	});
+	
+	
+	$.post('http://aposte.me/live/atualiza_live_bet365_REST.php', JSON.stringify(DATA), function(data) {console.log(data) });
+	
+});
+
+
