aa0bd783163a2c58445c8a8dc417afbafbd1ae55
ronaldoaf <ronaldo.a.f@gmail.com>
2017-02-24 15:37:53 -0300

    Update ahft.user.js

--- ahft.user.js
+++ ahft.user.js
@@ -70,7 +70,7 @@ unsafeWindow.jQuery.fn.extend({rclick:function(){var a=function(a,b){return Math
 function login(){
 	if($('.mmhdr-UserInfo_UserName').text()==''){
 		$('.hm-HeaderLinkLogin_Launcher').click();
-		$('#PopUp_UserName').val('LucasDM19');
+		$('#PopUp_UserName').val('rgoborin');
 		$('#PopUp_Password').val(localStorage.senha_bet365);
 		$('#PopUp_KML').val('on');
 		$('#LogInPopUpBttn').click();
