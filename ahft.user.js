c7c2b87bf4976991b3f4c82e92c1305d490d2959
ronaldoaf <ronaldo.a.f@gmail.com>
2017-03-19 15:57:28 -0300

    Update ahft.user.js

--- ahft.user.js
+++ ahft.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bot_AH_FT
 // @namespace    http://aposte.me/
-// @version      0.4.19
+// @version      0.4.20
 // @description  Utiliza ao vivo no Asian Handicap
 // @author       Ronaldo
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.16.4/lodash.min.js
@@ -104,7 +104,7 @@ bot.stake=function(){
 
         n_jogo=Number( GM_getValue('n_jogo') );
         if (n_jogo<=30.0) n_jogo=30.0;
-        percent=0.33/(n_jogo*0.10);  
+        percent=0.17/(n_jogo*0.10);  
       
     
 	
