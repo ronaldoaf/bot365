c1c519b04307c96c6b257f644e6a2d9e54d7debf
ronaldoaf <ronaldo.a.f@gmail.com>
2017-04-22 05:32:31 -0300

    Update totalcorner.user.js

--- totalcorner.user.js
+++ totalcorner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extrai Stats Totalcorner
 // @namespace    http://aposte.me
-// @version      0.1.8.1
+// @version      0.2
 // @description  Extrai Stats Totalcorner
 // @author       Ronlado Araújo de Farias
 // @match        http://www.totalcorner.com/match/today
@@ -26,64 +26,33 @@ $(document).ready(function(){
 	//Se não estiver aparecendo a columa "on target", define as colunas corretas
     if( $('th:contains(on target)').size()<1 ) {
         $.post('http://www.totalcorner.com/user/select_match_list_columns','asian_handicap=on&corner_score=on&total_corners=on&total_goals=on&dangerous_attack=on&shoot_on=on&shoot_off=on&events=on&save=today');
-    }
+    }	
 	
-	
-	
-	$( document ).ajaxComplete(function( event, res, settings) {
-		if (!settings.url.includes('api_ongoing_matches') ) return;
-		
-		var TABELA_JOGOS={};
-		$('[data-match_id]').each(function(i,e){  
-			TABELA_JOGOS[$(e).attr('data-match_id')]={
-				home: $(e).find('.match_home a').text(),
-				away: $(e).find('.match_away a').text()
-			};			
-		});
-		
-		
-		
+	setInterval(function(){
 		var DATA=[];
-		$(res.responseJSON).each(function(i,j){
-			
-			HA=$(j.han.split(',')).size()==2 ? (Number(j.han.split(',')[0]) + Number(j.han.split(',')[1]))/2.0 : Number(j.han.split(',')[0]);
-			try{
-				DATA.push({
-						id:   j.id,
-						time_:j.sta,
-						home: TABELA_JOGOS[j.id].home,
-						away: TABELA_JOGOS[j.id].away,
-						HA:   HA,
-						gH:   Number(j.hg),
-						gA:   Number(j.ag),
-						daH:  Number(j.hda), 
-						daA:  Number(j.ada), 
-						soH:  Number(j.ho), 
-						soA:  Number(j.ao), 
-						sfH:  Number(j.hf),
-						sfA:  Number(j.af)
-				});
-			} catch(e){
-				window.location.reload();
-			}
-			
-		});
-		
-		
-		
-		
+		$('.minutes_postfix').parents('tr').each(function(i,e){ 
+
+			var home=$(e).find('td:eq(3) a').text();
+			var away=$(e).find('td:eq(5) a').text();
+
+			DATA.push( {
+				id:   Number($(e).attr('data-match_id')), 
+				time_:$(e).find('.match_status_minutes').text(),
+				home: $(e).find('td:eq(3) a').text(), 
+				away: $(e).find('td:eq(5) a').text(), 
+				HA: -7.0,
+				gH: Number($(e).find('.match_goal').text().split(' - ')[0]),
+				gA: Number($(e).find('.match_goal').text().split(' - ')[1]),
+				daH: Number($(e).find('.match_attach').text().split(' - ')[0]),
+				daA: Number($(e).find('.match_attach').text().split(' - ')[1]),
+				soH: Number($(e).find('.match_on').text().split(' - ')[0]),
+				soA: Number($(e).find('.match_on').text().split(' - ')[1]),
+				sfH: Number($(e).find('.match_off').text().split(' - ')[0]),
+				sfA: Number($(e).find('.match_odff').text().split(' - ')[1])		
+
+			}); 
+		});	
 		$.post('http://aposte.me/live/atualiza_live_bet365_REST.php', JSON.stringify(DATA), function(data) {console.log(data) });
-		
-		/*
-		GM_xmlhttpRequest({
-			method: "POST",
-			url: "http://aposte.me/live/atualiza_live_bet365_REST.php",
-			data:  JSON.stringify(DATA),
-			onload: function(res){
-				console.log(res.responseText);
-			}
-		});
-		*/
-		
-	});
+	},15000);	
+
 });
