ba170fb030dbd0b4e824064cb451785b1b742395
Ronaldo Farias <ronaldo.a.@gmail.com>
2017-08-08 06:02:22 -0300

    Mundança Totalcorner

--- totalcorner.user.js
+++ totalcorner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extrai Stats Totalcorner
 // @namespace    http://aposte.me
-// @version      0.3.2
+// @version      0.4
 // @description  Extrai Stats Totalcorner
 // @author       Ronlado Araújo de Farias
 // @match        http://www.totalcorner.com/match/today
@@ -15,7 +15,7 @@ var toTimestamp=function(d){
 
 $(document).ready(function(){
 	
-	//Reinicia a pagina 30 segundos depois ser carregada
+	//Reinicia a pagina 20 segundos depois ser carregada
 	localStorage['reiniciado'] = localStorage['reiniciado'] || false;
 	setTimeout(function(){
 		if(JSON.parse(localStorage['reiniciado'])==false) {
@@ -25,11 +25,15 @@ $(document).ready(function(){
 		else{
 			localStorage['reiniciado']=false;			
 		}
-	},60000);
+	},20000);
 	
-	//Se não estiver aparecendo a columa "on target", define as colunas corretas
+	//Se não estiver aparecendo a columa "on target", refaz o login e define as colunas corretas
     if( $('th:contains(on target)').size()<1 ) {
-        $.post('http://www.totalcorner.com/user/select_match_list_columns','asian_handicap=on&corner_score=on&total_corners=on&total_goals=on&dangerous_attack=on&shoot_on=on&shoot_off=on&events=on&save=today');
+		$.post('http://www.totalcorner.com/user/login','password=rr842135&username=ronaldoaf');
+		setTimeout(function(){
+             $.post('http://www.totalcorner.com/user/select_match_list_columns','asian_handicap=on&corner_score=on&total_corners=on&total_goals=on&dangerous_attack=on&shoot_on=on&shoot_off=on&events=on&save=today');
+		},3000);
+        
     }	
 	
 	setInterval(function(){
@@ -46,14 +50,14 @@ $(document).ready(function(){
 				home: $(e).find('td:eq(3) a').text(), 
 				away: $(e).find('td:eq(5) a').text(), 
 				HA: -7.0,
-				gH: Number($(e).find('.match_goal').text().split(' - ')[0]),
-				gA: Number($(e).find('.match_goal').text().split(' - ')[1]),
-				daH: Number($(e).find('.match_attach').text().split(' - ')[0]),
-				daA: Number($(e).find('.match_attach').text().split(' - ')[1]),
-				soH: Number($(e).find('.match_on').text().split(' - ')[0]),
-				soA: Number($(e).find('.match_on').text().split(' - ')[1]),
-				sfH: Number($(e).find('.match_off').text().split(' - ')[0]),
-				sfA: Number($(e).find('.match_odff').text().split(' - ')[1])		
+				gH: Number($(e).find('.span_match_corner').text().split(' - ')[0]),
+				gA: Number($(e).find('.span_match_corner').text().split(' - ')[1]),
+				daH: Number($(e).find('.match_dangerous_attacks_div').text().split(' - ')[0]),
+				daA: Number($(e).find('.match_dangerous_attacks_div').text().split(' - ')[1]),
+				soH: Number($(e).find('.match_on_div').text().split(' - ')[0]),
+				soA: Number($(e).find('.match_on_div').text().split(' - ')[1]),
+				sfH: Number($(e).find('.match_off_div').text().split(' - ')[0]),
+				sfA: Number($(e).find('.match_off_div').text().split(' - ')[1])		
 
 			}); 
 		});	
